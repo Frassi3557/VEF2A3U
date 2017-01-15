56ea1578f26a523349656b81cca04800090c8006
Snorri Freyr Ásgeirsson <freysi_freys@live.com>
2017-01-15 20:10:47 +0000

    Add files via upload

---
+++ Verkefni 1/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Verkefni 1 - Digital Ocean</title>
+        <META HTTP-EQUIV="Pragma" CONTENT="no-cache">
+        <META HTTP-EQUIV="Expires" CONTENT="-1">
+        <style type="text/css">
+            body {
+                background-color:#36be8f;
+                color:#FFF;
+                border:0px;
+                margin:0px;
+                overflow:hidden;
+            }
+            .wrapper {
+                position:absolute;
+                top:15%;
+                left:0px;
+                right:0px;
+                width:100%;
+                height:auto;
+            }
+            .sammy {
+                float:left;
+                width:100%;
+                height:auto;
+                text-align:center;
+            }
+            .message {
+                float:left;
+                width:100%;
+                height:auto;
+                font-size:36px;
+                text-align:center;
+                font-family:Arial,Helvetica,sans-serif;
+                margin-top:25px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="sammy"><img src="https://assets.digitalocean.com/other_images/sammy.png"/></div>
+            <div class="message">Halló Heimur!<br><p>(Kv. Snorri)</p></div>
+        </div>
+    </body>
+</html>
